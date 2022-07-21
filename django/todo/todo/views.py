6b4b6305479a67c09f208bce7bb51e901cc96864
mkathuria41 <109571912+mkathuria41@users.noreply.github.com>
2022-07-21 14:53:38 +0530

    Update views.py

--- django/todo/todo/views.py
+++ django/todo/todo/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect, render
 from pytz import timezone
 from .models import alltodos
 #from .forms import PostForm
-# show all todo
+# show all todo and add todo
 def todo_list(request):
     todos = alltodos.objects.all()
     if request.method == "POST":
@@ -39,13 +39,7 @@ def update(request,id):
 
 # mark as complete
 def markcomplete(request,id):
-    db = alltodos.objects.all().filter(id=id)
-    todo= alltodos()
-    todo.id=id
-    todo.task=db[0].task
-    todo.description=db[0].description
-    todo.tag=db[0].tag
-    todo.created_date=db[0].created_date
+    todo = alltodos.objects.get(id=id)
     todo.Status=True
     todo.save()
     return redirect(todo_list)
@@ -53,13 +47,7 @@ def markcomplete(request,id):
 #mark as incomplete
 
 def markincompleted(request,id):
-    db = alltodos.objects.all().filter(id=id)
-    todo= alltodos()
-    todo.id=id
-    todo.task=db[0].task
-    todo.description=db[0].description
-    todo.tag=db[0].tag
-    todo.created_date=db[0].created_date
+    todo = alltodos.objects.get(id=id)
     todo.Status=False
     todo.save()
-    return redirect(todo_list)    
+    return redirect(todo_list)  
